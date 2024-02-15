71845dcd8b954458a0c0ca2b6d21b9f3dcfada1a
Tanner Davis <cholojuanito.dev@gmail.com>
2024-02-15 13:52:39 -0700

    Add "path" param to compile_pip.sh

--- scripts/compile_pip.sh
+++ scripts/compile_pip.sh
@@ -1,2 +1,7 @@
-#/bin/bash
-pip-compile requirements.in -q
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Please provide the path to the directory containing the 'requirements.in' file"
+    exit 1
+fi
+
+pip-compile "$1/requirements.in" -q
